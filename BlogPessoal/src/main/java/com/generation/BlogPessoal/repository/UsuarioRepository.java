f96277c3c6deeb7a7613e9c42ae950ed14064ca8
Lucas-schimit <lucas_schimit@outlook.com>
2021-03-03 13:51:44 -0300

    chore(Seguranca): Criado a camada a classe BasicSecurityConfig #1

--- BlogPessoal/src/main/java/com/generation/BlogPessoal/repository/UsuarioRepository.java
+++ BlogPessoal/src/main/java/com/generation/BlogPessoal/repository/UsuarioRepository.java
@@ -1,14 +1,14 @@
 package com.generation.BlogPessoal.repository;
 
-import java.lang.StackWalker.Option;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.BlogPessoal.model.Usuario;
-import com.sun.el.stream.Optional;
+
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	
-	public Optional<Usario> findByUsuario(String Usuario);
+	public Optional<Usuario> findByUsuario(String Usuario);
 
 }
