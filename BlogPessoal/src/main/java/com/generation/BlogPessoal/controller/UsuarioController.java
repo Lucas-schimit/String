c15da6695d1290ae956446a010e73d0f6e898bda
Lucas-schimit <lucas_schimit@outlook.com>
2021-04-07 17:34:15 -0300

    Fix(Atualização): Atualização do crud de atualização do Back-end

--- BlogPessoal/src/main/java/com/generation/BlogPessoal/controller/UsuarioController.java
+++ BlogPessoal/src/main/java/com/generation/BlogPessoal/controller/UsuarioController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.generation.BlogPessoal.model.Usuario;
 import com.generation.BlogPessoal.model.UsuarioLogin;
 import com.generation.BlogPessoal.service.UsuarioService;
+import com.generation.BlogPessoal.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping("/usuarios")
 @CrossOrigin("*")
 public class UsuarioController {
+	
+	@Autowired
+	private UsuarioRepository repository;
 	
 	@Autowired
 	private UsuarioService usuarioService;
@@ -29,12 +36,28 @@ public class UsuarioController {
 				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> GetById(@PathVariable long id){
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
+	
+	
 	@PostMapping("/cadastrar")
 	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario){
 		Optional<Usuario> user = usuarioService.CadastrarUsuario(usuario);
 		try {
 			return ResponseEntity.ok(user.get());
 		}catch (Exception e) {
+			return ResponseEntity.badRequest().build();
+		}
+	}
+	
+	@PutMapping
+	public ResponseEntity<Usuario> put(@RequestBody Usuario usuario){
+		Optional<Usuario> user = usuarioService.atualizarUsuario(usuario);
+		try {
+			return ResponseEntity.ok(user.get());
+		}catch (Exception e){
 			return ResponseEntity.badRequest().build();
 		}
 	}
--- BlogPessoal/src/main/java/com/generation/BlogPessoal/model/Postagens.java
+++ BlogPessoal/src/main/java/com/generation/BlogPessoal/model/Postagens.java
@@ -26,11 +26,11 @@ public class Postagens {
 	private long id;
 	
 	@NotNull
-	@Size(min=20, max=60)
+	@Size(min=5, max=60)
 	private String titulo;
 	
 	@NotNull
-	@Size(min=50, max=100)
+	@Size(min=5, max=100)
 	private String texto;
 	
 	@Temporal(TemporalType.TIMESTAMP)
